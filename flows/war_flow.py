d8aeb65a6dbf68f4d3f67f2a5786b2f77c3f71f4
Michael Fagan <michael.g.fagan@gmail.com>
2022-06-20 16:28:06 -0500

    Update war_flow

--- flows/war_flow.py
+++ flows/war_flow.py
@@ -1,9 +1,12 @@
-from prefect import Flow
-from tasks.bref_war import bref_war
-from tasks.fangraphs import fangraphs
+# from prefect import Flow
+# import buehrle_prefect.tasks.bref_war as bref_war
+# import buehrle_prefect.tasks.fangraphs as fangraphs
 
-with Flow("WAR") as flow:
-    bref_war()
-    fangraphs()
+# with Flow("WAR") as flow:
+#     bref_war.bref_war()
+#     fangraphs.fangraphs()
+#
+# flow.register(project_name="buehrle")
 
-flow.register(project_name="buehrle")
+import sys
+print(sys.path)
