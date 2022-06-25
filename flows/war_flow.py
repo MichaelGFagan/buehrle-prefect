9671a6f4cf936fec4a0785191f9e7f4be95b4294
Michael Fagan <michael.g.fagan@gmail.com>
2022-06-25 13:28:19 -0500

    Update war_flow for dbt job

--- flows/war_flow.py
+++ flows/war_flow.py
@@ -2,14 +2,37 @@ from prefect import Flow, Parameter
 import tasks.bref_war as bref_war
 import tasks.fangraphs as fangraphs
 import tasks.chadwick as chadwick
+import tasks.clone_dbt_repo as clone_dbt_repo
+import tasks.dbt_shell_task as dbt_shell_task
 
 
 with Flow("WAR") as flow:
-    chadwick.chadwick()
-    bref_war.bref_war()
+    chadwick = chadwick.chadwick()
+    bref_war = bref_war.bref_war()
     fangraphs_start = Parameter('fangraphs_start', default=2022)
     fangraphs_end = Parameter('fangraphs_end', default=2022)
-    fangraphs.fangraphs(start=fangraphs_start, end=fangraphs_end)
+    fangraphs = fangraphs.fangraphs(start=fangraphs_start, end=fangraphs_end)
+    clone_repo = clone_dbt_repo.clone_dbt_repo(
+        upstream_tasks=[chadwick, bref_war, fangraphs],
+    )
+    deps = dbt_shell_task.dbt(
+        command="dbt deps",
+        task_args={"name": "dbt deps"},
+        upstream_tasks=[clone_repo],
+    )
+    deps_output = dbt_shell_task.output_print(
+        deps,
+        task_args={"name": "dbt deps output"},
+    )
+    run = dbt_shell_task.dbt(
+        command="dbt run -m +wins_above_replacement",
+        task_args={"name": "dbt run"},
+        upstream_tasks=[deps_output]
+    )
+    run_output = dbt_shell_task.output_print(
+        run,
+        task_args={"name": "dbt run output"}
+    )
 
 
 def flow_register():
