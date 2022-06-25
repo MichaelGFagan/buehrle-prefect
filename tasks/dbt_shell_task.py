031e55ce0bcb28942bd680724d2b21139a801ccd
Michael Fagan <michael.g.fagan@gmail.com>
2022-06-25 14:36:53 -0500

    Use different auth method

--- tasks/dbt_shell_task.py
+++ tasks/dbt_shell_task.py
@@ -1,18 +1,38 @@
-import prefect
 from prefect import task
+from prefect.client import Secret
 from prefect.tasks.dbt.dbt import DbtShellTask
 
 
 dbt = DbtShellTask(
     return_all=True,
-    profiles_dir='~/.dbt/profiles.yml',
+    profile_name='buehrle-dbt',
+    overwrite_profiles=True,
     environment='dev',
     log_stdout=True,
     log_stderr=True,
+    dbt_kwargs={
+        "type": "bigquery",
+        "method": "service-account-json",
+        "project": Secret("BIGQUERY_PROJECT_ID"),
+        "dataset": Secret("BIGQUERY_DATASET"),
+        "threads": 4,
+        "keyfile_json": {
+            "type": "service_account",
+            "project_id": Secret("BIGQUERY_PROJECT_ID"),
+            "private_key_id": Secret("BIGQUERY_PRIVATE_KEY_ID"),
+            "private_key": Secret("BIGQUERY_PRIVATE_KEY"),
+            "client_email": Secret("BIGQUERY_CLIENT_EMAIL"),
+            "client_id": Secret("BIGQUERY_CLIENT_ID"),
+            "auth_uri": Secret("BIGQUERY_AUTH_URI"),
+            "token_uri": Secret("BIGQUERY_TOKEN_URI"),
+            "auth_provider_x509_cert_url": Secret("BIGQUERY_AUTH_PROVIDER_X509_CERT_URL"),
+            "client_x509_cert_url": Secret("BIGQUERY_CLIENT_X509_CERT_URL")
+        }
+    }
 )
 
 
-@task(trigger=all_finished)
+@task()
 def output_print(output):
     logger = prefect.context.get("logger")
     for o in output:
